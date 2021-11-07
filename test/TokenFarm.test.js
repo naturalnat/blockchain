87578ffca6465b236b37a4dafe3b7c0fcf704874
Natalie Taktachev <ntaktach@gmail.com>
2021-11-07 15:58:32 -0500

    mock dai test

---
+++ test/TokenFarm.test.js
@@ -0,0 +1,20 @@
+const { assert } = require('chai')
+
+const DaiToken = artifacts.require('DaiToken')
+const DappToken = artifacts.require('DappToken')
+const TokenFarm = artifacts.require('TokenFarm')
+
+require('chai')
+    .use(require('chai-as-promised'))
+    .should()
+
+contract('TokenFarm', (accounts) => {
+    //tests 
+    describe('Mock DAI deployment', async () => {
+        it('has a name', async () => { 
+            let daiToken = await DaiToken.new()
+            const name = await daiToken.name()
+            assert.equal(name, 'Mock DAI Token')
+        })
+    })
+})

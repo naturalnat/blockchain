1df531c636520e8baa35c7f572a045e1c7cab43c
Natalie Taktachev <ntaktach@gmail.com>
2021-11-07 16:06:36 -0500

    token transfer test

--- test/TokenFarm.test.js
+++ test/TokenFarm.test.js
@@ -10,6 +10,19 @@ require('chai')
 
 contract('TokenFarm', (accounts) => {
     //tests 
+    let daiToken, dappToken, tokenFarm
+
+    before(async () => {
+        //load contracts
+        let daiToken = await DaiToken.new()
+        let dappToken = await DappToken.new()
+        let tokenFarm = await TokenFarm.new(dappToken.address, daiToken.address)
+        
+        //tranfer all dapp tokens to farm 
+        await dappToken.transfer(tokenFarm.address, '1000000000000000000000000')
+   
+    })
+
     describe('Mock DAI deployment', async () => {
         it('has a name', async () => { 
             let daiToken = await DaiToken.new()
